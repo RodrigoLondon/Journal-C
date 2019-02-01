5bd340bcd294dfb553ad2d60ef19aca5da4526a5
Lewis Jones <lewisjones@lewiss-mbp>
2019-02-01 16:37:50 +0000

    Initial Commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Lewis Jones on 01/02/2019.
+//  Copyright © 2019 Rodrigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-C
+//
+//  Created by Lewis Jones on 01/02/2019.
+//  Copyright © 2019 Rodrigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
